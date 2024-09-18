9a4e22677b2e050e6c2d24d3767e76f4bbd3e18e
pablotrujilloelo@gmail.com <pablotrujilloelo@gmail.com>
2024-09-17 21:55:26 -0500

    first commit

---
+++ src/main/java/com/products_market/ProductsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.products_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/products_market/ProductsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.products_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
